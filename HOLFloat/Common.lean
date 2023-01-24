a28e3a9b670528f2c6798779acd5e8aef8d79bcb
ShaolunWang <wang.shaolun@gmail.com>
2023-01-24 20:14:06 +0000

    - Replacing Rational with Real - Update existing definitions - Finising Fixed.lean

--- HOLFloat/Common.lean
+++ HOLFloat/Common.lean
@@ -1,24 +1,21 @@
 import Mathlib
 
+def power (r : ℝ) (i : ℕ): ℝ :=
+  r ^ i
+noncomputable def ipow : (@& ℝ) → (@& ℤ) → ℝ
+  | x, Int.ofNat n => x ^ n
+  | x, Int.negSucc n =>  Inv.inv (x ^ n)
 
--- Should I define it this way?
-def npow : (@& ℚ) → (@& ℕ) → ℚ
-  | _, 0 => 1
-  | x, Nat.succ n => x ^ (Nat.succ n)
-def ipow : (@& ℚ) → (@& ℤ) → ℚ
-  | x, Int.ofNat n => npow x n
-  | x, Int.negSucc n => Rat.inv (npow x n)
-
-instance : HPow Rat Int Rat where
+noncomputable instance : HPow ℝ Int ℝ where
   hPow := ipow
 
-theorem ipow_lt_zero {r : ℚ}{i : ℤ} : 0 < r → 0 < r ^ i := by
+theorem ipow_lt_zero {r : ℝ}{i : ℤ} : 0 < r → 0 < r ^ i := by
   sorry
 
-theorem ipow_inv_neg {r : ℚ}{i : ℤ} : r ≠ 0 → r ^ i = Rat.inv (r ^(-i)) := by
+theorem ipow_inv_neg {r : ℝ}{i : ℤ} : r ≠ 0 → r ^ i = Inv.inv (r ^(-i)) := by
   sorry
 
-theorem ipow_add_exp {r : ℚ}(u : ℤ)(v : ℤ) : r ≠ 0 → r ^ u * r ^ v = r ^ (u + v) := by
+theorem ipow_add_exp {r : ℝ}(u : ℤ)(v : ℤ) : r ≠ 0 → r ^ u * r ^ v = r ^ (u + v) := by
 sorry
 
 /- 
@@ -26,37 +23,37 @@ theorem ipow_eq_exp
 theorem ipow_eq_exp_p
 -/
 
-theorem ipow_between {x : ℚ}{y z i : ℤ} : 
+theorem ipow_between {x : ℝ}{y z i : ℤ} : 
   (0 < x) → (y * x ^ e ≤ z * x ^ e) → (z * x ^ e ≤ (y + 1) * x ^ e) 
   → (z = y) ∨ (z = y + 1):= by
   sorry
-theorem ipow_to_one {r : ℚ} : r ^ 1 = r := by
+theorem ipow_to_one {r : ℝ} : r ^ 1 = r := by
   simp
 
-theorem ipow_to_zero {r : ℚ} : r ^ 0 = 1 := by
+theorem ipow_to_zero {r : ℝ} : r ^ 0 = 1 := by
   simp
 
-theorem ipow_le_one {r : ℚ}{i : ℤ} : 1 ≤ r → 0 ≤ i → 1 ≤ r ^ i := by
+theorem ipow_le_one {r : ℝ}{i : ℤ} : 1 ≤ r → 0 ≤ i → 1 ≤ r ^ i := by
   sorry
 
-theorem ipow_lt_one {r : ℚ}{i : ℤ} : 1 < r → 0 < i → 1 < r ^ i := by
+theorem ipow_lt_one {r : ℝ}{i : ℤ} : 1 < r → 0 < i → 1 < r ^ i := by
   sorry
-theorem ipow_le_sum {r : ℚ}{n : ℚ} : 2 ≤ r → 0 ≤ i → ∃ e : ℤ , n ≤ r ^ e := by
-  sorry
-
-theorem ipow_le_rat {r : ℚ}{z : ℚ} : 2 ≤ r → ∃ e : ℤ , z ≤ r ^ e := by
-  sorry
-theorem ipow_le_rat_two {r : ℚ}{z : ℚ} : 0 < z → 2 ≤ r → ∃ e : ℤ , r ^ e ≤ z := by
-  sorry
-theorem ipow_monotone {r : ℚ}{u : ℤ}{v : ℤ} : 2 ≤ r → r ^ u ≤ r ^ v → u ≤ v := by
-  sorry
-theorem ipow_monotone_two {r : ℚ}{u : ℤ}{v : ℤ} : 1 ≤ r → u ≤ v → r ^ u ≤ r ^ v := by
-  sorry
-theorem ipow_mul_inv_eq_one {r : ℚ}{i : ℤ} : 0 < r → r ^ i * r ^ (-i) = 1 := by
+theorem ipow_le_sum {r : ℝ}{n : ℝ} : 2 ≤ r → 0 ≤ i → ∃ e : ℤ , n ≤ r ^ e := by
   sorry
 
-def rerror {a : ℚ}{b : ℚ} :=
-  abs ((b - a)/ a)
+theorem ipow_le_rat {r : ℝ}{z : ℝ} : 2 ≤ r → ∃ e : ℤ , z ≤ r ^ e := by
+  sorry
+theorem ipow_le_rat_two {r : ℝ}{z : ℝ} : 0 < z → 2 ≤ r → ∃ e : ℤ , r ^ e ≤ z := by
+  sorry
+theorem ipow_monotone {r : ℝ}{u : ℤ}{v : ℤ} : 2 ≤ r → r ^ u ≤ r ^ v → u ≤ v := by
+  sorry
+theorem ipow_monotone_two {r : ℝ}{u : ℤ}{v : ℤ} : 1 ≤ r → u ≤ v → r ^ u ≤ r ^ v := by
+  sorry
+theorem ipow_mul_inv_eq_one {r : ℝ}{i : ℤ} : 0 < r → r ^ i * r ^ (-i) = 1 := by
+  sorry
 
-def closer {x y z : ℚ} :=
+noncomputable def rerror {a : ℝ}{b : ℝ}: ℝ :=
+  abs ((b - a) / a)
+
+def closer (x y z : ℝ): Prop :=
   abs (x - z) < abs (y - z)
--- HOLFloat/Fixed.lean
+++ HOLFloat/Fixed.lean
@@ -5,39 +5,78 @@ def is_valid_fformat (r p : Int) : Bool :=
   1 < r ∧ r % 2 == 0 ∧ 0 < p
 
 structure fformat where
-  r : ℚ
+  r : ℝ
   p : ℤ
   e : ℤ
-deriving Repr
 
 
 #check fformat.mk 1 2 3
 
-def is_frac (fmt : fformat)(x: ℚ)(f: ℤ) : Prop :=
+def is_frac (fmt : fformat)(x: ℝ)(f: ℤ) : Prop :=
   let e1 := fmt.r ^ (fmt.p - 1)
   let p1 := f ≤ e1
   let p2 := |x| = f * fmt.r ^ (fmt.e - fmt.p + 1)
   p1 ∧ p2
+
 open Classical
 
-noncomputable def ff (fmt: fformat)(x: ℚ) : ℤ :=
+noncomputable def ff (fmt: fformat)(x: ℝ) : ℤ :=
   Classical.epsilon (is_frac fmt x)
 
-def is_fixed (fmt: fformat)(x: ℚ) : Prop :=
+def is_fixed (fmt: fformat)(x: ℝ) : Prop :=
   ∃ f, is_frac fmt x f
-
-def is_fin_fixed (fmt: fformat)(x: ℚ) : Prop :=
+def is_fin_fixed (fmt: fformat)(x: ℝ) : Prop :=
   ∃ f, is_frac fmt x f ∧ f < fmt.r ^ (fmt.p - 1)
 
 -- fix point ulp
 
-def fulp (fmt: fformat) : ℚ :=
+noncomputable def fulp (fmt: fformat) : ℝ :=
   fmt.r ^ (fmt.e - fmt.p + 1)
 
-def finf (fmt: fformat) : ℚ :=
+noncomputable def finf (fmt: fformat) : ℝ :=
   fmt.r ^ fmt.e
 
-abbrev fixed (fmt: fformat) : Set ℚ :=
-  fun x: ℚ => is_fixed fmt x
+abbrev fixed (fmt: fformat) : Set ℝ :=
+  fun x: ℝ => is_fixed fmt x
 
-def is_ub (fmt : fformat) (x y : ℚ) : Prop := y ∈ fixed fmt ∧ x ≤ y
+#check fixed (fformat.mk 1 2 3)
+def is_ub (fmt : fformat) (x y : ℝ) : Prop :=
+  y ∈ fixed fmt ∧ x ≤ y
+
+def is_lub (fmt : fformat)(x y : ℝ): Prop :=
+  is_ub fmt x y ∧ (∀y₁, is_ub fmt x y₁ → y <= y₁)
+
+noncomputable def glb (fmt: fformat)(x: ℝ): ℝ:=
+  supₛ {y: ℝ | y ∈ (fixed fmt) ∧ y <= x}
+
+noncomputable def lub (fmt: fformat)(x: ℝ): ℝ:=
+  infₛ {y: ℝ | y ∈ (fixed fmt) ∧ y <= x}
+
+inductive roundmode where
+| to_near : roundmode
+| to_zero : roundmode
+| to_pinf : roundmode
+| to_ninf : roundmode
+#check roundmode.to_near
+noncomputable def fround (fmt: fformat)(mode : roundmode)(x: ℝ): ℝ :=
+  let lo := glb fmt x
+  let hi := lub fmt x
+  match mode with
+  | roundmode.to_near => 
+    if (closer lo hi x) then
+      lo
+    else if (closer hi lo x) then
+      hi
+    else if (ff (fmt) lo % 2 = 0) then
+      hi
+    else
+      hi
+  | roundmode.to_zero =>
+    if (0 ≤ x) then
+      glb fmt x
+    else
+      lub fmt x
+  | roundmode.to_pinf =>
+    lub fmt x
+  | roundmode.to_ninf =>
+    glb fmt x
